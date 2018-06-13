35c6c7a7ca728b1948805932158759883aeb76e3
gaoyr <375984181@qq.com>
2018-06-13 16:48:21 +0800

    BaseView 初稿

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # concise
+## 使用说明
+用于 一个Activity + 多个View展示。（一定是一个Activity ，如 MainActivity）
+好处是页面切换快，移除了Fragment可能带来的问题。
+###多View
+* 使用自定义栈进行管理进出顺序；
+* 继承统一父类BaseView；
+* 已为BaseView提供生命周期
+* 使用Map集合及软引用管理多View
+
+## BaseActivity
+提供View容器及 等待页面，错误页面
+
+## BaseView
+提供生命周期，返回键管理，具体View管理，软键盘管理
---
+++ app/src/main/java/com/gaoyanrong/concise/base/BaseView.java
@@ -0,0 +1,125 @@
+package com.gaoyanrong.concise.base;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.annotation.CallSuper;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import butterknife.ButterKnife;
+
+/**
+ * @author 高延荣
+ * @date 2018/6/13 11:25
+ * 描述: BaseView是一个View容器类，对外提供View布局，及生命周期
+ */
+public abstract class BaseView {
+    /**
+     * 上下文，在必要时，自行强转
+     */
+    protected Context mContext;
+    /**
+     * 当前View ID
+     */
+    protected String mId;
+    /**
+     * 重要，重要，重要！！！
+     * 非常重要！！！
+     * 用于数据的传递、获取，可根据数据变化进行不同操作
+     */
+    protected Bundle mBundle;
+    /**
+     * 资源
+     */
+    protected Resources mResources;
+    /**
+     * 填充
+     */
+    protected LayoutInflater mInflater;
+    /**
+     * 容器
+     */
+    protected View container;
+
+    public BaseView(Context context, String id, Bundle bundle) {
+        mContext = context;
+        mId = id;
+        mBundle = bundle;
+        mResources = context.getResources();
+        mInflater = LayoutInflater.from(context);
+
+        initContainer();
+        init();
+    }
+
+
+    /**
+     * 初始化容器
+     */
+    private void initContainer() {
+        container = mInflater.inflate(initLayoutID(), null);
+        ButterKnife.bind(this, container);
+    }
+
+    /////////////////////////// 子类实现 //////////////////////////////////////
+
+    /**
+     * 布局ID
+     * 在这里直接可以使用 ButterKnife获取对象
+     *
+     * @return layout
+     */
+    protected abstract int initLayoutID();
+
+    /**
+     * 类似于 Activity onCreate() ，做一些初始化动作。
+     */
+    protected abstract void init();
+
+
+    ////////////////////////////// 子类重写 生命周期 //////////////////////////////////////
+
+    /**
+     * 类似于 Activity onResume
+     */
+    @CallSuper
+    public void onResume() {
+    }
+
+    /**
+     * 类似于  Activity onPause
+     */
+    @CallSuper
+    public void onPause() {
+
+    }
+
+    /**
+     * 类似于 Activity 的 onNewIntent，当前View如果已存在，则
+     *
+     * @param bundle 传递数据
+     */
+    @CallSuper
+    public void onNewIntent(Bundle bundle) {
+
+    }
+
+    /**
+     * 是否接收回退动作，默认不接收
+     *
+     * @param keyCode int
+     * @param event   事件
+     * @return 是否拦截回退
+     */
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return false;
+    }
+
+
+    ////////////////////////////// 子类调用 //////////////////////////////////////
+
+
+
+}
